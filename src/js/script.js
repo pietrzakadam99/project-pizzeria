c9bb66d14347f72940d1afe8afaea73335315eb4
Adam Pietrzak <adampietrzak@Adams-MacBook-Air.local>
2021-03-19 20:50:41 +0100

    Added cart function

--- src/js/script.js
+++ src/js/script.js
@@ -316,6 +316,40 @@
       thisWidget.element.dispatchEvent(event);
     }
   }
+
+  class Cart{
+    constructor(element){
+      const thisCart = this;
+
+      thisCart.products = [];
+
+      thisCart.getElements(element);
+      thisCart.initActions();
+
+      console.log('new Cart', thisCart);
+    }
+
+    getElements(element){
+      const thisCart = this;
+
+      thisCart.dom = {};
+
+      thisCart.dom.wrapper = element;
+
+      /* dodaje definicje właściwości, która znajduje pojedynczy element o selektorze zapisanym w select.cart.toggleTrigger */
+      thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+    }
+
+    initActions(){
+      const thisCart = this;
+
+      thisCart.dom.toggleTrigger.addEventListener('click', function(event){
+        event.preventDefault();
+        thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+      });
+
+    }
+  }
   
 
 
@@ -346,7 +380,15 @@
 
       thisApp.initData();
       thisApp.initMenu();
+      thisApp.initCart();
     },
+
+    initCart: function(){
+      const thisApp = this;
+
+      const cartElem = document.querySelector(select.containerOf.cart);
+      thisApp.cart = new Cart(cartElem);
+    }
   };
 
   app.init();
